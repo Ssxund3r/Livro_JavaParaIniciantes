504ebeccedf4ab2cc85ace065f9ab64fddf1c4c6
Ssxund3r <gabrielfillip@gmail.com>
2022-07-20 00:01:49 -0300

    Tente Isto 1_1

---
+++ src/main/java/Capitulo1_FundamentosLinguagemJava/TenteIsto1_1_GalToLit.java
@@ -0,0 +1,16 @@
+package Capitulo1_FundamentosLinguagemJava;
+
+public class TenteIsto1_1_GalToLit {
+	public static void main(String[] args) {
+		
+		double gallons; //Contém o número de galões
+		double liters; // Contém a conversão para litros
+		
+		
+		gallons = 10; // Começa com 10 galões 
+		liters = gallons * 3.7854; // Converte para litros
+		
+		System.out.println(gallons + " gallons is " + liters + " liters.");
+		
+	}
+}
