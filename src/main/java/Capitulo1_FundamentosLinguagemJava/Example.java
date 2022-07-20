319eba8fd7c325973b6f8603cde63691a655f55f
Ssxund3r <gabrielfillip@gmail.com>
2022-07-19 22:47:58 -0300

    Primeiro programa simples

---
+++ src/main/java/Capitulo1_FundamentosLinguagemJava/Example.java
@@ -0,0 +1,13 @@
+package Capitulo1_FundamentosLinguagemJava;
+
+/*Você seguirá estas três etapas: 
+ *1. Insira o programa;
+ *2. Compile o programa;
+ *3. Execute o programa.*/
+public class Example {
+	public static void main(String[] args) {
+		
+		System.out.println("Java drives the web! ");
+		
+	}
+}
