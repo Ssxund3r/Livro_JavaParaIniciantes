82bffe271350d9579323c8a24e935bb8b0c81c9e
Ssxund3r <gabrielfillip@gmail.com>
2022-07-21 09:17:31 -0300

    exercício - InchToMeterTable

---
+++ src/main/java/Capitulo1_FundamentosLinguagemJava/InchToMeterTable.java
@@ -0,0 +1,24 @@
+package Capitulo1_FundamentosLinguagemJava;
+
+public class InchToMeterTable {
+	public static void main(String[] args) {
+
+		double inches, meters;
+		int counter;
+
+		counter = 0;
+
+		for (inches = 0; inches <= 144; inches++) {
+			meters = inches / 39.37;
+			System.out.println(inches + " inches is " + meters + " meters. ");
+
+			counter++;
+			//a cada 12 linhas, exibe uma linha em branco
+			if (counter == 12) {
+				System.out.println();
+				counter = 0; // zera o contador de linhas
+			}
+		}
+
+	}
+}
