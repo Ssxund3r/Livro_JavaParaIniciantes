07a1fffa23e1b940e0a825f05c7bd7537a0f389d
Ssxund3r <gabrielfillip@gmail.com>
2022-07-20 23:34:20 -0300

    BlockDemo

---
+++ src/main/java/Capitulo1_FundamentosLinguagemJava/BlocoDemo.java
@@ -0,0 +1,19 @@
+package Capitulo1_FundamentosLinguagemJava;
+
+/*Demonstra um bloco de código.*/
+public class BlocoDemo {
+	public static void main(String[] args) {
+
+		double i, j, d;
+
+		i = 5;
+		j = 10;
+
+		// O alvo desta instrução if é um bloco de código
+		if (i != 0) {
+			System.out.println("i does not equal zero");
+			d = j / i;
+			System.out.println("j / i is " + d);
+		}
+	}
+}
