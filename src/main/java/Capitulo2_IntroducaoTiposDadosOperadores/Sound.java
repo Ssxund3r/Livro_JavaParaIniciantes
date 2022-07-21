516c82827ebf127137a0262026b97153e51d33e0
Ssxund3r <gabrielfillip@gmail.com>
2022-07-21 11:45:53 -0300

    Sound - exemplo

---
+++ src/main/java/Capitulo2_IntroducaoTiposDadosOperadores/Sound.java
@@ -0,0 +1,23 @@
+package Capitulo2_IntroducaoTiposDadosOperadores;
+
+/*1. Crie um novo programa chamado Sound.java;
+ *
+ *2. Para calcular a distância de tempo, 7,2 tem um componentefracionário
+ *Embora pudéssemos usar um valor do tipo float, usaremos double no exemplo;
+ *
+ *3. Para fazer o cálculo, vocÊ multiplicará 7,2 por 1.100. Em seguida, atribuirá 
+ *esse valor a uma variável;
+ *
+ *4. Por fim, exibirá o resultado.
+ **/
+public class Sound {
+	public static void main(String[] args) {
+
+		double dist;
+
+		dist = 7.2 * 1100;
+
+		System.out.println("The lightning is " + dist + " feet way.");
+
+	}
+}
