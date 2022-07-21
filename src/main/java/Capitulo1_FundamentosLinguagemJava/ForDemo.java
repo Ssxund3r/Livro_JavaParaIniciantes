7d0b62adcc376e122905a3300545bab4d437aa7a
Ssxund3r <gabrielfillip@gmail.com>
2022-07-20 23:24:42 -0300

    ForDemo

---
+++ src/main/java/Capitulo1_FundamentosLinguagemJava/ForDemo.java
@@ -0,0 +1,15 @@
+package Capitulo1_FundamentosLinguagemJava;
+
+/*Demonstra laço for*/
+public class ForDemo {
+	public static void main(String[] args) {
+
+		int count;
+
+		for (count = 0; count < 5; count++) { // Este laço itera cinco vezes
+			System.out.println("This is count: " + count);
+		}
+
+		System.out.println("Done! ");
+	}
+}
