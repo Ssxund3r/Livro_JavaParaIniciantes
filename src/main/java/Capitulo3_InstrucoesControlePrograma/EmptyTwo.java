8c091eb9d0bc2c9f7d8e2c6c0ff47d6bdf22ecc7
Ssxund3r <gabrielfillip@gmail.com>
2022-07-21 23:43:41 -0300

    EmptyTwo - exemplo

---
+++ src/main/java/Capitulo3_InstrucoesControlePrograma/EmptyTwo.java
@@ -0,0 +1,15 @@
+package Capitulo3_InstrucoesControlePrograma;
+
+//Retira mais uma parte do laço for
+public class EmptyTwo {
+	public static void main(String[] args) {
+
+		int i = 0;
+	
+		for (; i < 10;) {
+			System.out.println("Pass # " + i);
+			i++; //Incrementa a variável de controle de laço
+		}
+
+	}
+}
