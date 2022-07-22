4d9dc44d409280751fcea3c683ee66e9c7593d17
Ssxund3r <gabrielfillip@gmail.com>
2022-07-22 11:08:49 -0300

    WhileDemo

---
+++ src/main/java/Capitulo3_InstrucoesControlePrograma/WhileDemo.java
@@ -0,0 +1,18 @@
+package Capitulo3_InstrucoesControlePrograma;
+
+//Demostra o laço while
+public class WhileDemo {
+	public static void main(String[] args) {
+
+		char ch;
+
+		ch = 'a';
+
+		// exibe o alfabeto usando o laço while
+		while (ch <= 'z') {
+			System.out.println(ch);
+			ch++;
+		}
+
+	}
+}
