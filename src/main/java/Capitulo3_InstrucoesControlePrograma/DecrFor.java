b52e72bafab1d6f82bf88aa673b4b09a17d58122
Ssxund3r <gabrielfillip@gmail.com>
2022-07-21 23:27:41 -0300

    DecrFor - exemplo

---
+++ src/main/java/Capitulo3_InstrucoesControlePrograma/DecrFor.java
@@ -0,0 +1,13 @@
+package Capitulo3_InstrucoesControlePrograma;
+
+//Um laço for sendo executado em sentido negativo
+public class DecrFor {
+	public static void main(String[] args) {
+
+		int x;
+
+		for (x = 100; x > -100; x -= 5) 
+			System.out.println(x);
+		
+	}
+}
