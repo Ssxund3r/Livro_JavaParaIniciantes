1a3aae77a33cef9915d9bfdd860c17927a245a28
Ssxund3r <gabrielfillip@gmail.com>
2022-07-22 23:53:12 -0300

    ContoToLabel - exemplo

--- src/main/java/Capitulo3_InstrucoesControlePrograma/ContDemo.java
+++ src/main/java/Capitulo3_InstrucoesControlePrograma/ContDemo.java
@@ -4,7 +4,8 @@ package Capitulo3_InstrucoesControlePrograma;
 public class ContDemo {
 	public static void main(String[] args) {
 		int i;
-
+		
+		//exibe os números pares entre 0 e 100
 		for (i = 0; i <= 100; i++) {
 			if ((i % 2) != 0)
 				continue; // iteração
---
+++ src/main/java/Capitulo3_InstrucoesControlePrograma/ContoToLabel.java
@@ -0,0 +1,17 @@
+package Capitulo3_InstrucoesControlePrograma;
+
+//Usa coninue com um rótulo
+public class ContoToLabel {
+	public static void main(String[] args) {
+
+		outerloop: for (int i = 0; i < 10; i++) {
+			System.out.print("\nOuter loop pass " + i + ", Inner loop: ");
+			for (int j = 1; j < 10; j++) {
+				if (j == 5)
+					continue outerloop; // laço externo de continue
+				System.out.print(j);
+			}
+		}
+
+	}
+}
