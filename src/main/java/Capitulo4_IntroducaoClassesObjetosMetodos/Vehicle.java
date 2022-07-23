b128ec2ee8ca9806258c69381181093a8e48cb30
Ssxund3r <gabrielfillip@gmail.com>
2022-07-23 10:53:44 -0300

    Vehicle - exemplo

---
+++ src/main/java/Capitulo4_IntroducaoClassesObjetosMetodos/Vehicle.java
@@ -0,0 +1,11 @@
+package Capitulo4_IntroducaoClassesObjetosMetodos;
+//Um programa que usa a classe Vehicle.
+public class Vehicle {
+	public static void main(String[] args) {
+		
+		int passengers;	//número de passageiros 
+		int fuelCap; //capacidade de armazenamento de combustível em galões 
+		int mpg; // consumo de combustível em milhas por galão
+		
+	}
+}
