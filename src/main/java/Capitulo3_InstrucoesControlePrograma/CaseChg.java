030e756b38f8f51f69bbe46219779a25b46d1870
Ssxund3r <gabrielfillip@gmail.com>
2022-07-23 09:55:26 -0300

    CaseChg

---
+++ src/main/java/Capitulo3_InstrucoesControlePrograma/CaseChg.java
@@ -0,0 +1,28 @@
+package Capitulo3_InstrucoesControlePrograma;
+
+import java.io.IOException;
+
+//Altera a caixa das letras
+public class CaseChg {
+	public static void main(String[] args) throws IOException {
+
+		char ch;
+		int changes = 0;
+
+		System.out.println("Enter period to stop.");
+
+		do {
+			ch = (char) System.in.read();
+			if (ch >= 'a' & ch <= 'z') {
+				ch -= 32;
+				changes++;
+				System.out.println(ch);
+			} else if (ch >= 'A' & ch <= 'Z') {
+				ch += 32;
+				changes++;
+				System.out.println(ch);
+			}
+		} while (ch != '.');
+		System.out.println("Case changes: " + changes);
+	}
+}
---
+++ src/main/java/Capitulo3_InstrucoesControlePrograma/Progress.java
@@ -0,0 +1,10 @@
+package Capitulo3_InstrucoesControlePrograma;
+//Usa o laço for para gerar a progressão
+public class Progress {
+	public static void main(String[] args) {
+		
+		for(int i = 1; i < 100; i += i)
+			System.out.println(i + " ");
+		
+	}
+}
