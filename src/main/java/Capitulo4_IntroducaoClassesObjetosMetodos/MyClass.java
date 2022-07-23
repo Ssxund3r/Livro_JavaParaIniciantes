c5ac89ebd29735fe95592c36cd94632b9c1d3f70
Ssxund3r <gabrielfillip@gmail.com>
2022-07-23 16:57:21 -0300

    ConsDemo - exemplo

---
+++ src/main/java/Capitulo4_IntroducaoClassesObjetosMetodos/MyClass.java
@@ -0,0 +1,13 @@
+package Capitulo4_IntroducaoClassesObjetosMetodos;
+
+//Um construtor simples
+public class MyClass {
+
+	int x;
+
+	// Este é o construtor de MyClass
+	MyClass() {
+		x = 10;
+	}
+
+}
