f15e580704d456fd14ffac43a74c0fa468bff6cd
Ssxund3r <gabrielfillip@gmail.com>
2022-07-23 14:46:29 -0300

    CompFuel - exemplo

--- src/main/java/Capitulo4_IntroducaoClassesObjetosMetodos/CompFuel.java
+++ src/main/java/Capitulo4_IntroducaoClassesObjetosMetodos/CompFuel.java
@@ -1,5 +1,30 @@
 package Capitulo4_IntroducaoClassesObjetosMetodos;
 
 public class CompFuel {
+	public static void main(String[] args) {
+		Vehicle minivan = new Vehicle();
+		Vehicle sportscar = new Vehicle();
 
+		double gallons;
+		int dist = 252;
+
+		// atribui valores a campos de minivan
+		minivan.passengers = 7;
+		minivan.fuelCap = 16;
+		minivan.mpg = 21;
+
+		// atribui valroes a campos de sportscar
+		sportscar.passengers = 2;
+		sportscar.fuelCap = 14;
+		sportscar.mpg = 12;
+
+		gallons = minivan.fuelNeeded(dist);
+
+		System.out.println("To go " + dist + " miles minivan needs " + gallons + " gallons of fuel.");
+
+		gallons = sportscar.fuelNeeded(dist);
+
+		System.out.println("To go " + dist + " miles sportscar needs " + gallons + " gallons of fuel.");
+
+	}
 }
