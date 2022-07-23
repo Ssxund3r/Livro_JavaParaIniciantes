6209ae5beb1c30167bf7024b2ca3b3138bd37c41
Ssxund3r <gabrielfillip@gmail.com>
2022-07-23 14:14:25 -0300

    Factor - exemplo

---
+++ src/main/java/Capitulo4_IntroducaoClassesObjetosMetodos/Factor.java
@@ -0,0 +1,13 @@
+package Capitulo4_IntroducaoClassesObjetosMetodos;
+
+public class Factor {
+
+	boolean isFactor(int a, int b) {
+		if ((b % a) == 0)
+			return true;
+		else
+			return false;
+
+	}
+
+}
