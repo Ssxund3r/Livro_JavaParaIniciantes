21916a4d26a451dec37df506442b0433dea939f4
Ssxund3r <gabrielfillip@gmail.com>
2022-07-22 22:34:01 -0300

    BreakFiveDemo - exemplo

---
+++ src/main/java/Capitulo3_InstrucoesControlePrograma/BreakFiveDemo.java
@@ -0,0 +1,21 @@
+package Capitulo3_InstrucoesControlePrograma;
+
+//Outro exemplo do uso de break com um rótulo
+public class BreakFiveDemo {
+	public static void main(String[] args) {
+
+		done: for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				for (int k = 0; k < 10; k++) {
+					System.out.println(k + " ");
+					if (k == 5)
+						break done; // desvia para done
+				}
+				System.out.println("After k loop"); // não será executado
+			}
+			System.out.println("After j loop");
+		}
+		System.out.println("After i loop");
+
+	}
+}
