2ab46012f238b411a669f63c2fd130dc85b73ad4
Ssxund3r <gabrielfillip@gmail.com>
2022-07-23 18:33:08 -0300

    FDemo - exemplo

--- src/main/java/Capitulo4_IntroducaoClassesObjetosMetodos/Finalize.java
+++ src/main/java/Capitulo4_IntroducaoClassesObjetosMetodos/Finalize.java
@@ -2,6 +2,17 @@ package Capitulo4_IntroducaoClassesObjetosMetodos;
 
 public class Finalize {
 	public static void main(String[] args) {
-		
+		int count;
+
+		FDemo ob = new FDemo(0);
+
+		/*
+		 * Agora, gere um grande número de obejtos. Em alguns momento, a coleta de lixo
+		 * ocorrerá. Nota: você pode ter de aumentar o número de obejtos gerados para
+		 * forçar a coleta de lixo.
+		 */
+		for (count = 1; count < 100000; count++)
+			ob.generator(count);
+
 	}
 }
