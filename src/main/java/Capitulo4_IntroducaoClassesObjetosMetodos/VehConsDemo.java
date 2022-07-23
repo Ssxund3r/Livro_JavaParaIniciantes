9ac221d1131749841ed91ecc48f39a59feda3369
Ssxund3r <gabrielfillip@gmail.com>
2022-07-23 17:19:01 -0300

    VehConsDemo - exemplo

---
+++ src/main/java/Capitulo4_IntroducaoClassesObjetosMetodos/VehConsDemo.java
@@ -0,0 +1,21 @@
+package Capitulo4_IntroducaoClassesObjetosMetodos;
+
+public class VehConsDemo {
+	public static void main(String[] args) {
+
+		// Constrói veículos completos
+		Vehicle minivan = new Vehicle(7, 16, 21);
+		Vehicle sportscar = new Vehicle(2, 14, 12);
+		double gallons;
+		int dist = 252;
+		
+		gallons = minivan.fuelNeeded(dist);
+		
+		System.out.println("To go " + dist + " miles minivan needs " + gallons + " gallons of fuel. ");
+		
+		gallons = sportscar.fuelNeeded(dist);
+		
+		System.out.println("To go " + dist + " miles sportscar needs " + gallons + " gallons of fuel. ");
+
+	}
+}
