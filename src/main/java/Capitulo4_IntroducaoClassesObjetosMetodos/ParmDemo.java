14f68ea58a89a847d65b38f6cfe35a449f4dad4c
Ssxund3r <gabrielfillip@gmail.com>
2022-07-23 13:54:30 -0300

    ParmDemo - exemplo

---
+++ src/main/java/Capitulo4_IntroducaoClassesObjetosMetodos/ParmDemo.java
@@ -0,0 +1,16 @@
+package Capitulo4_IntroducaoClassesObjetosMetodos;
+
+//
+public class ParmDemo {
+	public static void main(String[] args) {
+		ChkNum e = new ChkNum();
+
+		if (e.isEven(10))
+			System.out.println("10 is even");
+		if (e.isEven(9))
+			System.out.println("9 is even");
+		if (e.isEven(8))
+			System.out.println("8 is even");
+
+	}
+}
