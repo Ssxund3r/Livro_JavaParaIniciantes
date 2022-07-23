b06009afea79a4f94baf17712b534cd0981672bb
Ssxund3r <gabrielfillip@gmail.com>
2022-07-23 17:07:34 -0300

    VehicleConst - exemplo

--- src/main/java/Capitulo4_IntroducaoClassesObjetosMetodos/Vehicle.java
+++ src/main/java/Capitulo4_IntroducaoClassesObjetosMetodos/Vehicle.java
@@ -7,6 +7,18 @@ public class Vehicle {
 	int fuelCap; // capacidade de armazenamento de combustível em galões
 	int mpg; // consumo de combustível em milhas por galão
 
+	// Esse é um construtor para Vehicle.
+	Vehicle(int p, int f, int m) {
+		passengers = p;
+		fuelCap = f;
+		mpg = m;
+	}
+	
+	//Exibe construtor vazio para não gerar erro nas classes anteriores
+	Vehicle() {
+
+	}
+
 	// Exibe a autonomia void
 	void range() {
 		System.out.println("Range is " + fuelCap * mpg);
