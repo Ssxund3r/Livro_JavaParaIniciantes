5500f43d921362d0ee8965fcf1cc7a9968f392e6
Ssxund3r <gabrielfillip@gmail.com>
2022-07-24 20:32:21 -0300

    QueueDemo - exemplo

---
+++ src/main/java/Capitulo5_MaisTiposDadosOperadores/QueueDemo.java
@@ -0,0 +1,47 @@
+package Capitulo5_MaisTiposDadosOperadores;
+
+public class QueueDemo {
+	public static void main(String[] args) {
+
+		Queue bigQ = new Queue(100);
+		Queue smallQ = new Queue(4);
+		char ch;
+		int i;
+
+		System.out.println("Using bigQ to store the alfabet.");
+		// insere alguns números em bigQ
+		for (i = 0; i < 26; i++)
+			bigQ.put((char) ('A' + i));
+
+		// recupera e exibe elementos de bigQ
+		System.out.println("Contents of bigQ: ");
+		for (i = 0; i < 26; i++) {
+			ch = bigQ.get();
+			if (ch != (char) 0)
+				System.out.print(ch + " ");
+		}
+
+		System.out.println("\n");
+
+		System.out.println("Using smallQ to generate errors.");
+		// Agora, usa smallQ para gerar alguns erros
+		for (i = 0; i < 5; i++) {
+			System.out.print("Attempting to store " + (char) ('Z' - i));
+
+			smallQ.put((char) ('Z' - i));
+
+			System.out.println();
+		}
+		System.out.println();
+
+		// mais erros em smallQ
+		System.out.print("Contents of smallQ: ");
+		for (i = 0; i < 5; i++) {
+			ch = smallQ.get();
+
+			if (ch != (char) 0)
+				System.out.print(ch);
+		}
+
+	}
+}
