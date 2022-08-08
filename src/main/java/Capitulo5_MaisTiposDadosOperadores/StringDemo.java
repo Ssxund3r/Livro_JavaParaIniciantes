1ce4bd156fbd712280cff0d5c6a8685986032a1a
Ssxund3r <gabrielfillip@gmail.com>
2022-08-08 15:52:05 -0300

    StringDemo - exemplo

---
+++ src/main/java/Capitulo5_MaisTiposDadosOperadores/StringDemo.java
@@ -0,0 +1,16 @@
+package Capitulo5_MaisTiposDadosOperadores;
+
+//Introduz String
+public class StringDemo {
+	public static void main(String[] args) {
+		// declara strings de várias maneiras
+		String str1 = new String("Java strings are objects.");
+		String str2 = new String("They are constructed various ways.");
+		String str3 = new String(str2);
+
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+
+	}
+}
