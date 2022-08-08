150a29ccf75f71976c22b454c66e668d5e64fd56
Ssxund3r <gabrielfillip@gmail.com>
2022-08-08 18:02:52 -0300

    StrOps - exemplo

---
+++ src/main/java/Capitulo5_MaisTiposDadosOperadores/StrOps.java
@@ -0,0 +1,46 @@
+package Capitulo5_MaisTiposDadosOperadores;
+
+//Algumas operações com Strings.
+public class StrOps {
+	public static void main(String[] args) {
+		String str1 = "When it comes to web programming, Java is #1! ";
+		String str2 = new String(str1);
+		String str3 = "Java String are powerful!";
+		int result, idx;
+		char ch;
+
+		System.out.println("Lenght of str1: " + str1.length());
+
+		// exibe um caractere de cada vez de str1
+		for (int i = 0; i < str1.length(); i++)
+			System.out.print(str1.charAt(i));
+		System.out.println();
+
+		if (str1.equals(str2))
+			System.out.println("str1 equals str2! ");
+		else
+			System.out.println("str1 does not equal str2! ");
+
+		if (str1.equals(str3))
+			System.out.println("str1 equals str3");
+		else
+			System.out.println("str1 does not equal str3s");
+
+		result = str1.compareTo(str3);
+		if (result == 0)
+			System.out.println("str1 and str3 are equal");
+		else if (result < 0)
+			System.out.println("str1 is less than str3");
+		else
+			System.out.println("str1 is greater than str3");
+
+		// atribui um novo string a str2
+		str2 = "One two three one";
+
+		idx = str2.indexOf("One");
+		System.out.println("Index of firts occurrence of one: " + idx);
+		idx = str2.lastIndexOf("One");
+		System.out.println("Index of last occurrence of One: " + idx);
+
+	}
+}
