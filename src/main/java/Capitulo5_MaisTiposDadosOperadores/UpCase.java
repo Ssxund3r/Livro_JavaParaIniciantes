81807d28384130d2a6d62a1756316953aa3c408b
Ssxund3r <gabrielfillip@gmail.com>
2022-08-09 17:25:24 -0300

    UpCase - exemplo

---
+++ src/main/java/Capitulo5_MaisTiposDadosOperadores/UpCase.java
@@ -0,0 +1,20 @@
+package Capitulo5_MaisTiposDadosOperadores;
+
+//Letras maiúsculas
+public class UpCase {
+	public static void main(String[] args) {
+
+		char ch;
+
+		for (int i = 0; i < 10; i++) {
+			ch = (char) ('a' + i);
+			System.out.print(ch);
+
+			// Essa instrução desativa o 6o bit.
+			ch = (char) ((int) ch & 65503); // agora ch é maiúscula.
+
+			System.out.print(ch + " ");
+		}
+
+	}
+}
