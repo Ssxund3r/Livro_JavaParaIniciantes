da3fea82981667c1e4e3d44b3e03932b73e963e5
Ssxund3r <gabrielfillip@gmail.com>
2022-08-12 13:39:00 -0300

    Err - exemplo

--- src/main/java/Capitulo6_VerificacaoMinuciosaMetodosClasses/ErrMsg.java
+++ src/main/java/Capitulo6_VerificacaoMinuciosaMetodosClasses/ErrMsg.java
@@ -2,8 +2,11 @@ package Capitulo6_VerificacaoMinuciosaMetodosClasses;
 
 public class ErrMsg {
 	public static void main(String[] args) {
-		
-		
-		
+
+		ErrorMsg err = new ErrorMsg();
+
+		System.out.println(err.gerErrorMsg(2));
+		System.out.println(err.gerErrorMsg(19));
+
 	}
 }
